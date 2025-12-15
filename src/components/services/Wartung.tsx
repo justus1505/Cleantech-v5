e48d8993d040e3b9e982eb5b9e31d2e41beaadac
justus1505 <justus.schmidt.online@gmail.com>
2025-12-15 11:25:40 +0100

    Update Wartung.tsx

--- src/components/services/Wartung.tsx
+++ src/components/services/Wartung.tsx
@@ -1,6 +1,6 @@
 import { useNavigate } from 'react-router-dom';
 import { ArrowLeft, Wrench, Settings, Hammer, Sparkles, Palette, Phone, Mail, ChevronDown } from 'lucide-react';
-import logo from '@/assets/dcb51da292f54aef6e88b5901410915a0793c7.png';
+import ... from '@assets/dcb51da292ff54aef6e88b5901410915a0793c7.png';
 import maintenanceImg from '@/assets/4025f99136ffd5ef97de51d0aa6dd294dfd06af9.png';
 import { useEffect, useState } from 'react';
 import { Footer } from '../Footer';
